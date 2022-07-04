10034bc7f4c721b6c522e32764fef5f1cb8b4a7a
Amr_Yasser <amr.yasser@intellisc.com>
2022-07-04 15:22:46 +0200

    adding graphql setup && express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({ graphiql: true }));
+
+app.listen(4000, () => {
+  console.log("Listening");
+});
